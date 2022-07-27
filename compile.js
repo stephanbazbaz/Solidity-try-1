d6be2a74673317ef635e05baad51cd0ee56c441b
stephanbazbaz <stephan@pwm.co.il>
2022-07-27 10:20:07 +0300

    tests

--- compile.js
+++ compile.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
-
+//https://rinkeby.infura.io/v3/ebf667f0828b45b7b35d776e53cce44d
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8')
 module.exports =  solc.compile(source, 1).contracts[':Inbox'];
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,15 +5,29 @@ const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require("../compile");
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there!'
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
     .send({ from: accounts[0], gas: "1000000" });
 });
 describe("Inbox", () => {
+
   it("deployes a contract", () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address)
   });
+  it('has a defual msg', async () => {
+    const msg = await inbox.methods.message().call()
+    assert.equal(msg, INITIAL_STRING)
+  })
+  it('sets a msg', async () => {
+    await inbox.methods.setMessage('Bye there').send({
+      from: accounts[0]
+    })
+    const msg = await inbox.methods.message().call();
+    assert.equal(msg, 'Bye there')
+  })
+
 });
